0e72044acb4eca3a48fe360d4840cccacc770fd1
Francisco Júnior <franciscojn49@gmail.com>
2026-05-10 20:50:55 -0300

    feat(web): commented TanStackRouterDevtools

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,12 +1,12 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Layout } from "../layout";
 
 export const Route = createRootRoute({
   component: () => (
     <Layout>
       <Outlet />
-      <TanStackRouterDevtools />
+      {/* <TanStackRouterDevtools /> */}
     </Layout>
   ),
 });
