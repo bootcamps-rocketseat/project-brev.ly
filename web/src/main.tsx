c85d09f546b5fce818bde24c904db343005759c9
Francisco Júnior <franciscojn49@gmail.com>
2026-05-10 11:42:45 -0300

    feat(web): configured message in toast

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,12 +4,16 @@ import "./theme.css";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
 
 const router = createRouter({
   routeTree,
 });
+
+const TOAST_AUTO_CLOSE_TIME_IN_MILLISECONDS = 1000 * 5;
+const TOAST_LIMIT = 5;
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -21,6 +25,13 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ToastContainer
+        autoClose={TOAST_AUTO_CLOSE_TIME_IN_MILLISECONDS}
+        limit={TOAST_LIMIT}
+        position="bottom-right"
+        hideProgressBar
+        closeButton={false}
+      />
     </QueryClientProvider>
   </StrictMode>,
 );
--- web/src/pages/home/hooks/use-form-new-link.tsx
+++ web/src/pages/home/hooks/use-form-new-link.tsx
@@ -5,13 +5,17 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { services } from "@/services";
 import { env } from "@/env";
 import { LinkType } from "@/types";
+import type { AxiosError } from "axios";
+import { toast } from "react-toastify";
 
 const newFormLinkSchema = zod.object({
   originalUrl: zod.url("Informe uma url válida"),
   shortenedUrl: zod
     .string()
-    .min(3, "Informe um link encurtado")
-    .regex(/^[a-zA-Z0-9-]+$/, "Use apenas letras, números e hífen"),
+    .regex(
+      /^[a-zA-Z0-9_-]+$/,
+      "Informe uma url minúscula e sem espaço/caracter especial.",
+    ),
 });
 
 export type NewFormLinkData = zod.infer<typeof newFormLinkSchema>;
@@ -33,7 +37,30 @@ export const useFormNewLink = () => {
       form.reset();
       queryClient.invalidateQueries({ queryKey: [LinkType.LIST_LINKS] });
     },
-    onError: () => {},
+    onError: ({ response, status }: AxiosError) => {
+      if (status === 400) {
+        const data = response?.data as {
+          message: string;
+        };
+
+        const message =
+          data.message === "Shortened URL already exists"
+            ? "O link encurtado já existe."
+            : data.message;
+
+        toast.error(
+          <div className="flex flex-col">
+            <p>Erro no cadastro</p>
+            <span>{message}</span>
+          </div>,
+          {
+            className: "!bg-red-100 !text-red-600",
+          },
+        );
+
+        form.setError("shortenedUrl", { message });
+      }
+    },
   });
 
   const handleSubmit = (data: NewFormLinkData) => {
